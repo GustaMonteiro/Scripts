aa315b55720347cc8d50386444e210617635fda6
Gustavo Monteiro <gcvm6070@gmail.com>
2022-04-05 15:34:09 -0300

    Added jpg to png

--- convert-jpg-png.sh
+++ convert-jpg-png.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 DIRETORIO=$1
-CAMINHOS=$(find $DIRETORIO -type f -iname \*.png)
+CAMINHOS=$(find $DIRETORIO -type f -iname \*.jpg)
 
 for caminho in $CAMINHOS; do
     NOME_IMAGEM=$(ls $caminho | rev | cut -d/ -f 1 | rev | cut -d. -f 1)
     DIRETORIO_IMAGEM=$(ls $caminho | rev | cut -d/ -f 2- | rev)
 
-    convert $caminho $DIRETORIO_IMAGEM/$NOME_IMAGEM.jpg
+    convert $caminho $DIRETORIO_IMAGEM/$NOME_IMAGEM.png
 done
